5f12daeffb25e3412849813b3b4ded12b4d177d4
Renata Peeva <renatapeeva4@gmail.com>
2024-07-12 09:53:54 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/teamprojects/repository/ProjectTopicRepository.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.wp.teamprojects.repository;
+
+import mk.ukim.finki.wp.teamprojects.model.Professor;
+import mk.ukim.finki.wp.teamprojects.model.ProjectTopic;
+import mk.ukim.finki.wp.teamprojects.model.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProjectTopicRepository extends JpaSpecificationRepository<ProjectTopic, Long> {
+    Optional<ProjectTopic> findByTitle(String title);
+    Optional<ProjectTopic> findByStudentsContaining(Student student);
+    List<ProjectTopic> findAllByProfessor(Professor professor);
+}
